36041c4e3bb53fe948e4c3814171c3a9a2f070cd
Adesh Gupta <adesh02092000@gmail.com>
2022-01-20 18:53:26 +0530

    Added comments

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,6 @@
 console.log("Hello from the service worker");
 // Service worker is acts as an client-side server and hence makes the web app work offline
+// Service worker runs on its own thread, apart from the main thread. Hence we are not loading it from the
+// html instead created a sw-register file to that
+
+// Also after some time of inactivity this thread gets deactivated
