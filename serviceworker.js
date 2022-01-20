80977d516aa84f2bcab8f464c042e773242daacc
Adesh Gupta <adesh02092000@gmail.com>
2022-01-20 19:12:42 +0530

    cached html and font

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,13 @@
-const assets = ["styles.css", "app.js", "sw-register.js"];
+// "/" == index.html
+const assets = [
+  "/",
+  "styles.css",
+  "app.js",
+  "sw-register.js",
+  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
 
-// Installing the serviceWorker
+// Installing the serviceWorker, and adding all the assets in the cache
 this.addEventListener("install", (event) => {
   caches.open("assets").then((cache) => {
     cache.addAll(assets);
