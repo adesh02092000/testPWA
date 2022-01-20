53e59f337406cc13a4bb2f304a5598dfd2b13fc4
Adesh Gupta <adesh02092000@gmail.com>
2022-01-20 19:08:00 +0530

    Install service worker and add all the assets in the browser cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,11 @@
-console.log("Hello from the service worker");
-// Service worker is acts as an client-side server and hence makes the web app work offline
-// Service worker runs on its own thread, apart from the main thread. Hence we are not loading it from the
-// html instead created a sw-register file to that
+const assets = ["styles.css", "app.js", "sw-register.js"];
 
-// Also after some time of inactivity this thread gets deactivated
+// Installing the serviceWorker
+this.addEventListener("install", (event) => {
+  caches.open("assets").then((cache) => {
+    cache.addAll(assets);
+  });
+});
+
+// Service workers has a local cache, which is use to store some or all of the resources
+// You can prefetch these resources in the cache or just like web apps cache them on request
