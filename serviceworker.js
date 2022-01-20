a1c908409a277caa4ce9d33c37e7eb4c4d00c9e4
Adesh Gupta <adesh02092000@gmail.com>
2022-01-20 19:42:44 +0530

    serving all assets

--- serviceworker.js
+++ serviceworker.js
@@ -7,16 +7,26 @@ const assets = [
   "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
 ];
 
-// Installing the serviceWorker, and adding all the assets in the cache
 this.addEventListener("install", (event) => {
-  caches.open("assets").then((cache) => {
-    cache.addAll(assets);
-  });
+  event.waitUntil(
+    caches.open("assets").then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
 
-// Serving with the service worker, not each URL is rendering this same response, even the HTML is not
-// rendered. To bypass this use shift + refresh
 this.addEventListener("fetch", (event) => {
-  const response = new Response("Hello I'm a response");
-  event.respondWith(response);
+  event.respondWith(
+    caches
+      .match(event.request) // searching in the cache
+      .then((response) => {
+        if (response) {
+          // The request is in the cache
+          return response; // cache hit
+        } else {
+          // We need to go to the network
+          return fetch(event.request); // cache miss
+        }
+      })
+  );
 });
